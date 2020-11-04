e8ffbd9cb954a14dd88c3a0c66b8aae371c511f6
Vinson Cernuto <cernutovinson@gmail.com>
2020-11-04 17:50:11 -0500

    deploy

--- server.js
+++ server.js
@@ -15,15 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+mongoose.connect(MONGODB_URI);
+
 // routes
 app.use(require("./routes/api.js"));
 
